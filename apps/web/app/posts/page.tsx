986635bd3bc6a86aa6975eda4c05c6b85421696a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-09-13 16:27:27 +0630

    feat: add post

---
+++ apps/web/app/posts/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React, {FormEvent, useState} from 'react';
+import axios from "axios";
+
+const PostList = () => {
+        const [title, setTitle] = useState('')
+
+
+        function onPostCreate(e: FormEvent<HTMLFormElement>) {
+            e.preventDefault()
+
+            axios.post('http://localhost:4000/posts', {
+                title
+            }).then((res) => {
+                console.log(res.data)
+            })
+        }
+
+        return (
+            <div>
+                <h1>Create Post</h1>
+                <form onSubmit={onPostCreate}>
+                    <input type="text" onChange={e => setTitle(e.target.value)}/>
+                    <button type={"submit"}>Create Post</button>
+                </form>
+            </div>
+        );
+    }
+;
+
+export default PostList;
